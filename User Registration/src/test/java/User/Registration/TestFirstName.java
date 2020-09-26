c816b721647125940f9c4b29858098a9b33c7cd3
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-09-26 23:38:55 +0530

    File Updated

--- User Registration/src/test/java/User/Registration/TestFirstName.java
+++ User Registration/src/test/java/User/Registration/TestFirstName.java
@@ -11,7 +11,7 @@ import java.util.*;
 //@RunWith(Parameterized.class)
 public class TestFirstName {
 
-	//@Test
+	@Test
 	public void when_Valid_First_Name_Returns_True() {
 		System.out.println("Enter valid first name down below: ");
 		Scanner sc = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class TestFirstName {
 		Assert.assertEquals(true, firstName);
 	}
 	
-	//@Test
+	@Test
 	public void when_Valid_Last_Name_Returns_True() {
 		System.out.println("Enter valid last name down below: ");
 		Scanner sc = new Scanner(System.in);
@@ -33,7 +33,7 @@ public class TestFirstName {
 		Assert.assertEquals(true, lastName);
 	}
 	
-	//@Test
+	@Test
 	public void when_Valid_Email_Returns_True() {
 		System.out.println("Enter valid email id down below: ");
 		Scanner sc = new Scanner(System.in);
@@ -44,7 +44,7 @@ public class TestFirstName {
 		Assert.assertEquals(true, email);
 	}
 	
-	//@Test
+	@Test
 	public void when_Valid_Mobile_Number_Returns_True() {
 		System.out.println("Enter valid mobile number down below: ");
 		Scanner sc = new Scanner(System.in);
@@ -55,7 +55,7 @@ public class TestFirstName {
 		Assert.assertEquals(true, email);
 	}
 	
-	//@Test
+	@Test
 	public void when_Valid_Password_Returns_True() {
 		System.out.println("Enter valid password down below: ");
 		Scanner sc = new Scanner(System.in);
